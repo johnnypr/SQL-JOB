a6d035aba136a091641b2f16329debfe0bf53e0d
Johnny Perez <johnnypr11@icloud.com>
2019-10-14 00:05:28 -0500

    Organization

---
+++ Excel Sheets/PROJECT 1/idCheck.py
@@ -0,0 +1,16 @@
+from openpyxl import Workbook
+import tqdm
+import xlrd
+
+
+def findIDS(file1,file2):
+    loc1 = (file1)
+    loc2 = (file2)
+    
+
+
+
+
+if __name__ == "__main__":
+    findIDS("test.xlsx","Confirmed Firm IDs in USPTO Data.xlsx")
+    
